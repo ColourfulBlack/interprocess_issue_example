021d28404528f429a0d2e634d751666269208c85
ColourfulBlack <kalicock@outlook.com>
2023-05-08 22:47:32 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Cloudtoid.Interprocess;
+using Microsoft.Extensions.Logging;
+
+var factory = new QueueFactory();
+var options = new QueueOptions(
+    queueName: "sample-queue", bytesCapacity: 24);
+using var publisher = factory.CreatePublisher(options);
